1ffc9f00c10a8aee7e2c868e361926feed8b28f3
username <email@example.com>
2024-03-07 21:42:19 +1000

    commented out button

--- expo_sample/hello-world/App.js
+++ expo_sample/hello-world/App.js
@@ -1,20 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, SafeAreaView, StyleSheet, View, Pressable } from 'react-native';
 
 export default function App() {
+  const getButStyle = ({pressed})=>
+    pressed ? [styles.pressed, styles.button] : [styles.button];
+
   return (
-    <View style={styles.container}>
-      <Text>Hey this is your first app sick!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.paragraph}>
+        Hello world!!
+      </Text>
+      <View style={styles.horizontalLine} />
+      <Text style={styles.paragraph}>
+        From Group 9
+      </Text>
+      {/* <Pressable
+        style={getButStyle}
+        onPress={() => {
+          ct++;
+          console.log(`I am clicked ${ct} times`);
+        }}
+      >
+      <Text>Button</Text>
+      </Pressable> */}
+
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: "center",
+    backgroundColor: '#ecf0f1',
+    padding: 8,
   },
+  paragraph: {
+    margin: 8,
+    fontSize: 18,
+    fontWeight: 'regular',
+    textAlign: 'center',
+  },
+  horizontalLine: {
+    height: 2, // Thickness of the line
+    width: '30%', // Length of the line; adjust as needed
+    backgroundColor: '#000', // Color of the line
+  },
+  button:{
+    backgroundColor: "#99f",
+    padding: 5,
+    borderWidth: 1,
+    borderColor: '#000',
+    borderRadius: 5,
+  },
+  pressed:{
+    opacity: 0.5,
+  }
 });
